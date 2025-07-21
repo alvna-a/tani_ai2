2f93a0e44aafa9252312b2b41a83c1893fe2fc56
alvna-a <alvinaaputriaulia@gmail.com>
2025-07-21 21:13:40 +0700

    Move Flutter Web build to root for deployment

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7566024615e72eeee863a925787b5a19
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "27667a1e41073bd02b49403fd013e5f9",
+"flutter_bootstrap.js": "e7ad9405faf18069bdd3541d09174f83",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
